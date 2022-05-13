51da598f8da15f689cc639654c811cfaaa557985
Alexander Jährling <jaehrling@gmx.eu>
2022-05-13 23:26:29 +0200

    Initial Commit

---
+++ TableCellsInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TableCellsInSwiftUI
+//
+//  Created by Alexander Jährling on 13.05.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TableCellsInSwiftUI/TableCellsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TableCellsInSwiftUIApp.swift
+//  TableCellsInSwiftUI
+//
+//  Created by Alexander Jährling on 13.05.22.
+//
+
+import SwiftUI
+
+@main
+struct TableCellsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
